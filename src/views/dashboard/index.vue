43f9a4711a240299886cfefd8a29d295243f015c
yunfan <yunfan@tsign.cn>
2021-03-22 12:30:07 +0800

    upall

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="dashboard-container">
+    <component :is="currentRole" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import uploadExcel from './upload-excel'
+import page404 from '../error-page/404'
+
+export default {
+  name: 'Dashboard',
+  components: { uploadExcel, page404 },
+  data() {
+    return {
+      currentRole: 'uploadExcel'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'roles'
+    ])
+  },
+  created() {
+    if (!this.roles.includes('admin')) {
+      this.currentRole = 'page404'
+    }
+  }
+}
+</script>
---
+++ src/views/puppeteer/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="puppeteer-container"></div>
+</template>
+
+<script>
+// impor
+export default {
+  name: 'puppeteer',
+  data() {
+    return {}
+  },
+  watch: {},
+  created() {},
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      // const puppeteer = require('puppeteer')
+      // (async() => {
+      //   const browser = await puppeteer.launch({headless: false})
+      //   const page = await browser.newPage()
+      //   await page.goto('https://www.dianxiaomi.com/dxmCommodityProduct/index.htm', {waitUntil: 'networkidle'})
+      //   //
+
+        
+      //   // console.log($('.pull-right .btn-dropdown .button')[1], 1111)
+      //   //await page.screenshot({path: './data/zhentaoo/zhentaoo.png', type: 'png'});
+
+      //   // browser.close();
+      // })()
+    }
+  },
+  destroyed() {}
+}
+</script>
+
+<style lang="scss" scoped>
+.puppeteer-container {
+  
+}
+</style>
